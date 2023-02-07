9bff8b5e0b00eb954dfd039a3168f04e8b4af386
o.nasteka <o.nasteka@gmarkg.com>
2023-02-07 14:23:12 +0200

    featureWebHook fix

--- src/config/webhook-client.php
+++ src/config/webhook-client.php
@@ -6,7 +6,7 @@ return [
             'name' => 'webhook-pool-argentisnano',
             'signing_secret' => env('WEBHOOK_CLIENT_SECRET_FOR_POOL_ARGENTISNANO'),
             'signature_header_name' => 'X-signature-pool-argentisnano',
-            'signature_validator' => \App\Jobs\Webhook\ArgentisnanoValidator::class,
+            'signature_validator' => \App\Jobs\Webhook\ArgentisNanoValidator::class,
             'webhook_profile' => \Spatie\WebhookClient\WebhookProfile\ProcessEverythingWebhookProfile::class,
             'webhook_response' => \Spatie\WebhookClient\WebhookResponse\DefaultRespondsTo::class,
             'webhook_model' => \Spatie\WebhookClient\Models\WebhookCall::class,
