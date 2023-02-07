439b24d626480a0fc47765771ed6dc2619fdd33d
o.nasteka <o.nasteka@gmarkg.com>
2023-02-07 19:16:18 +0200

    featureLeads
    updates

--- src/app/Http/Requests/LeadsRequest.php
+++ src/app/Http/Requests/LeadsRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use App\Models\Lead;
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreLeads extends FormRequest
+class LeadsRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- src/app/Jobs/Webhook/HandlerForPoolArgentisNano.php
+++ src/app/Jobs/Webhook/HandlerForPoolArgentisNano.php
@@ -2,8 +2,9 @@
 
 namespace App\Jobs\Webhook;
 
-use App\Http\Requests\StoreLeads;
+use Illuminate\Http\Request;
 use App\Models\Lead;
+use Illuminate\Support\Facades\Log;
 use Spatie\WebhookClient\Jobs\ProcessWebhookJob;
 
 class HandlerForPoolArgentisNano extends ProcessWebhookJob
@@ -15,12 +16,13 @@ class HandlerForPoolArgentisNano extends ProcessWebhookJob
      */
     public $timeout = 120;
 
-    public function handle(StoreLeads $request)
+    public function handle(Request $request)
     {
-        logger('handle request: ', $request->all());
+//        logger('handle request: ', $this->request->all());
+        Log::info('handle request: ', var_dump($request->all()));
 
-        $lead = Lead::create(($request->only(array_keys($request->rules()))));
-        logger('lead:',$lead);
+//        $lead = Lead::create(($request->only(array_keys($request->rules()))));
+//        logger('lead:',$request);
 
     }
 }
