67bda9c30afbc02195ec19a627a03b765e3831c1
o.nasteka <o.nasteka@gmarkg.com>
2023-02-08 13:44:01 +0200

    featureLeads
    log

--- src/app/Jobs/Webhook/HandlerForPoolArgentisNano.php
+++ src/app/Jobs/Webhook/HandlerForPoolArgentisNano.php
@@ -2,13 +2,12 @@
 
 namespace App\Jobs\Webhook;
 
-use Illuminate\Http\Request;
 use App\Models\Lead;
-use Illuminate\Support\Facades\Log;
 use Spatie\WebhookClient\Jobs\ProcessWebhookJob;
 
 class HandlerForPoolArgentisNano extends ProcessWebhookJob
 {
+
     /**
      * The number of seconds the job can run before timing out.
      *
@@ -16,13 +15,10 @@ class HandlerForPoolArgentisNano extends ProcessWebhookJob
      */
     public $timeout = 120;
 
-    public function handle(Request $request)
+
+    public function handle()
     {
-//        logger('handle request: ', $this->request->all());
-        Log::info('handle request: ', var_dump($request->all()));
-
-//        $lead = Lead::create(($request->only(array_keys($request->rules()))));
-//        logger('lead:',$request);
-
+        $data = json_decode($this->webhookCall, true)['payload'];
+        logger($data);
     }
 }
